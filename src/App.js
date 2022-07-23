53bb8e4c838b5cd9b8cb536cec762a75a2d47f2a
JuliaJulia <ulana_m@ukr.net>
2022-07-23 11:49:11 +0300

    hw-5 10%

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import {Link, Route, Routes} from "react-router-dom";
+import Todos from "./pages/Todos";
+import Albums from "./pages/Albums";
+import Comments from "./pages/Comments";
 
 function App() {
   return (
     <div>
-text
+
+        <div>
+
+          <h2>menu</h2>
+          <li><Link to={'/todos'}>TODOS</Link></li>
+          <li><Link to={'/albums'}>ALBUMS</Link></li>
+          <li><Link to={'/comments'}>COMMENTS</Link></li>
+        </div>
+<div>
+    <Routes>
+        <Route path={'/todos'} element={<Todos/>}/>
+        <Route path={'/albums'} element={<Albums/>}/>
+        <Route path={'/comments'} element={<Comments/>}/>
+    </Routes>
+
+
+</div>
+
+
     </div>
   );
 }
